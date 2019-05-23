c2a13b402fb4224b1b0729e63ff841cfe235dc13
{YOUR NAME HERE} <{YOUR EMAIL ADDRESS}>
2019-05-23 16:02:39 -0700

    added font display support for fonts collection
    formatting changes
    added font display support for fonts collection
    
    refactored code
    add font display support for fonts collection
    
    made things more robust
    add font display support
    
    refactored code
    dd font display support for fonts collection
    
    refactored code
    
    refactored code
    
    added spacing
    
    refactored

--- spec/helpers/getFontsCollection.js
+++ spec/helpers/getFontsCollection.js
@@ -5,7 +5,7 @@ const Lab = require('lab'),
       testRunner = require('../spec-helpers').testRunner;
 
 describe('getFontsCollection', function () {
-    it('should return the expected font link', function (done) {
+    it('should return a font link with fonts from theme settings and &display=swap when no font-display value is passed', function (done) {
         const themeSettings = {
             'test1-font': 'Google_Open+Sans',
             'test2-font': 'Google_Open+Sans_400italic',
@@ -22,12 +22,53 @@ describe('getFontsCollection', function () {
         runTestCases([
             {
                 input: '{{getFontsCollection}}',
-                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:,400italic,700|Karla:700|Lora:400|Volkron:|Droid:400,700|Crimson+Text:400,700" rel="stylesheet">',
+                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:,400italic,700|Karla:700|Lora:400|Volkron:|Droid:400,700|Crimson+Text:400,700&display=swap" rel="stylesheet">',
             },
         ], done);
     });
+    it('should return a font link with fonts from theme settings and &display=swap when font-display value passed is invalid', function (done) {
+        const themeSettings = {
+            'test1-font': 'Google_Open+Sans',
+            'test2-font': 'Google_Open+Sans_400italic',
+            'test3-font': 'Google_Open+Sans_700',
+            'test4-font': 'Google_Karla_700',
+            'test5-font': 'Google_Lora_400_sans',
+            'test6-font': 'Google_Volkron',
+            'test7-font': 'Google_Droid_400,700',
+            'test8-font': 'Google_Crimson+Text_400,700_sans',
+            'random-property': 'not a font'
+        };
 
-    it('should not crash if a malformed Google font is passed', function (done) {
+        const runTestCases = testRunner({themeSettings});
+        runTestCases([
+            {
+                input: '{{getFontsCollection font-display="oreo"}}',
+                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:,400italic,700|Karla:700|Lora:400|Volkron:|Droid:400,700|Crimson+Text:400,700&display=swap" rel="stylesheet">',
+            },
+        ], done);
+    });
+    it('should return a font link with fonts from theme settings and &display=${font-display} when valid font-display value is passed', function (done) {
+        const themeSettings = {
+            'test1-font': 'Google_Open+Sans',
+            'test2-font': 'Google_Open+Sans_400italic',
+            'test3-font': 'Google_Open+Sans_700',
+            'test4-font': 'Google_Karla_700',
+            'test5-font': 'Google_Lora_400_sans',
+            'test6-font': 'Google_Volkron',
+            'test7-font': 'Google_Droid_400,700',
+            'test8-font': 'Google_Crimson+Text_400,700_sans',
+            'random-property': 'not a font'
+        };
+
+        const runTestCases = testRunner({themeSettings});
+        runTestCases([
+            {
+                input: '{{getFontsCollection font-display="fallback"}}',
+                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:,400italic,700|Karla:700|Lora:400|Volkron:|Droid:400,700|Crimson+Text:400,700&display=fallback" rel="stylesheet">',
+            },
+        ], done);
+    });
+    it('should not crash if a malformed Google font is passed when no font-display value is passed', function (done) {
         const themeSettings = {
             'test1-font': 'Google_Open+Sans',
             'test2-font': 'Google_',
@@ -38,7 +79,37 @@ describe('getFontsCollection', function () {
         runTestCases([
             {
                 input: '{{getFontsCollection}}',
-                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:" rel="stylesheet">',
+                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:&display=swap" rel="stylesheet">',
+            },
+        ], done);
+    });
+    it('should not crash if a malformed Google font is passed when valid font-display value is passed', function (done) {
+        const themeSettings = {
+            'test1-font': 'Google_Open+Sans',
+            'test2-font': 'Google_',
+            'test3-font': 'Google'
+        };
+
+        const runTestCases = testRunner({themeSettings});
+        runTestCases([
+            {
+                input: '{{getFontsCollection font-display="fallback"}}',
+                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:&display=fallback" rel="stylesheet">',
+            },
+        ], done);
+    });
+    it('should not crash if a malformed Google font is passed when invalid font-display value is passed', function (done) {
+        const themeSettings = {
+            'test1-font': 'Google_Open+Sans',
+            'test2-font': 'Google_',
+            'test3-font': 'Google'
+        };
+
+        const runTestCases = testRunner({themeSettings});
+        runTestCases([
+            {
+                input: '{{getFontsCollection font-display="oreo"}}',
+                output: '<link href="https://fonts.googleapis.com/css?family=Open+Sans:&display=swap" rel="stylesheet">',
             },
         ], done);
     });
