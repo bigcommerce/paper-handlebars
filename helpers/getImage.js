12cd87b571d56bd214af4ab5bdd24442e0fe5a64
Nathan Booker <nathan.booker@bigcommerce.com>
2019-07-07 11:31:59 -0500

    Return image URL as SafeString

--- helpers/getImage.js
+++ helpers/getImage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var _ = require('lodash');
+const SafeString = require('handlebars').SafeString;
+const common = require('./lib/common.js');
 
 const factory = globals => {
     return function(image, presetName, defaultImageUrl) {
@@ -11,7 +13,8 @@ const factory = globals => {
         var width;
         var height;
 
-        if (!_.isPlainObject(image) || !_.isString(image.data) || image.data.indexOf('{:size}') === -1) {
+        if (!_.isPlainObject(image) || !_.isString(image.data)
+            || !common.isValidURL(image.data) || image.data.indexOf('{:size}') === -1) {
             // return empty string if not a valid image object
             defaultImageUrl = defaultImageUrl ? defaultImageUrl : '';
             return _.isString(image) ? image : defaultImageUrl;
@@ -19,8 +22,8 @@ const factory = globals => {
 
         if (_.isPlainObject(presets) && _.isPlainObject(presets[presetName])) {
             // If preset is one of the given presets in _images
-            width = parseInt(presets[presetName].width, 10) || 4096;
-            height = parseInt(presets[presetName].height, 10) || 4096;
+            width = parseInt(presets[presetName].width, 10) || 5120;
+            height = parseInt(presets[presetName].height, 10) || 5120;
             size = width + 'x' + height;
 
         } else if (sizeRegex.test(settings[presetName])) {
@@ -31,8 +34,7 @@ const factory = globals => {
             size = 'original';
         }
 
-        return image.data.replace('{:size}', size);
-
+        return new SafeString(image.data.replace('{:size}', size));
     };
 };
 
