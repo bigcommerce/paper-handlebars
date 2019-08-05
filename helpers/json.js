0ffd5a1b96e48e2945275be56d3863187a40957f
Nathan Booker <nathan.booker@bigcommerce.com>
2019-08-05 12:46:15 -0500

    Fix json helper to work with a nested getImage helper

--- helpers/json.js
+++ helpers/json.js
@@ -1,7 +1,11 @@
 'use strict';
+const SafeString = require('handlebars').SafeString;
 
 const factory = () => {
     return function(data) {
+        if (data instanceof SafeString) {
+            data = data.toString();
+        }
         return JSON.stringify(data);
     };
 };
