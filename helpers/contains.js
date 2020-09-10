a652c0c9990e0cf80757ac59ba90d116f0f7e178
Patrick Edelman <patrick.edelman@bigcommerce.com>
2020-09-10 12:57:41 -0700

    STRF-8666: Adds test coverage for contains helper

--- helpers/contains.js
+++ helpers/contains.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Is any value included in a collection or a string?
+ * Is any value included in a collection or a string or top level value in an object
  *
  * @example
  * {{#contains fonts "Roboto"}} ... {{/contains}}
@@ -10,7 +10,7 @@
 const factory = () => {
     return function(container, value) {
         const options = arguments[arguments.length - 1];
-        const contained = container.includes(value);
+        const contained = containerIncludesValue(container, value);
 
         // Yield block if true
         if (contained) {
@@ -19,6 +19,28 @@ const factory = () => {
             return options.inverse(this);
         }
     };
+};
+
+const containerIncludesValue = (container, value) => {
+    switch (typeof container) {
+        case 'string':
+            return container.includes(value);
+        case 'object':
+            if (container) {
+                if  (Array.isArray(container)) {
+                    return container.includes(value);
+                }
+
+                // Check if the supplied value matches a top level value in the container object
+                return Object.values(container).some(objValue => {
+                    if (objValue && value) {
+                        return JSON.stringify(objValue) === JSON.stringify(value);
+                    }
+                });
+            }
+        default:
+            return false;
+    }
 };
 
 module.exports = [{
