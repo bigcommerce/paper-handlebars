500dd94544b027b60f84882dbb2222b254b4ce24
Juned Kazi <juned.kazi@bigcommerce.com>
2020-09-11 13:55:31 -0700

    Validate collection passed to pluck is array

--- spec/helpers/pluck.js
+++ spec/helpers/pluck.js
@@ -7,9 +7,10 @@ const Lab = require('lab'),
 describe('pluck helper', function() {
     const context = {
         users: [
-          { 'user': 'barney', 'age': 36 },
-          { 'user': 'fred',   'age': 40 }
-        ]
+          { 'user': 'barney', 'age': 36, 'image': { 'url': 'barney.jpg' } },
+          { 'user': 'fred',   'age': 40, 'image': { 'url': 'fred.jpg' }  }
+        ],
+        user_obj: { 'user': 'fred',   'age': 40, 'image': { 'url': 'fred.jpg' }  }
     };
 
     const runTestCases = testRunner({context});
@@ -23,6 +24,42 @@ describe('pluck helper', function() {
             {
                 input: '{{#each (pluck users "user")}}hello {{this}} {{/each}}',
                 output: 'hello barney hello fred ',
+            },
+            {
+                input: '{{pluck users "image.url"}}',
+                output: 'barney.jpg,fred.jpg',
+            },
+            {
+                input: '{{pluck users "image.alt"}}',
+                output: '',
+            },
+            {
+                input: '{{pluck "" "age"}}',
+                output: '',
+            },
+            {
+                input: '{{pluck undefined}}',
+                output: '',
+            },
+            {
+                input: '{{pluck null}}',
+                output: '',
+            },
+            {
+                input: '{{pluck}}',
+                output: '',
+            },
+            {
+                input: '{{pluck user_obj "user"}}',
+                output: 'fred',
+            },
+            {
+                input: '{{pluck user_obj "name"}}',
+                output: '',
+            },
+            {
+                input: '{{pluck user_obj "image.url"}}',
+                output: 'fred.jpg',
             },
         ], done);
     });
