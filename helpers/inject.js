76055c51f9e8b24924d1cda8694150a2bdac8652
Juned Kazi <junedkazi@users.noreply.github.com>
2020-11-17 10:15:16 -0800

    Revert "STRF-8585 Escape injected values"

--- helpers/inject.js
+++ helpers/inject.js
@@ -1,33 +1,6 @@
 'use strict';
 
 const factory = globals => {
-    function filterValues(value) {
-        let result = value;
-        try {
-            JSON.parse(value);
-        } catch (e) {
-            if (typeof value === 'string') {
-                result = globals.handlebars.escapeExpression(value);
-            }
-            if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-                result = filterObjectValues(value);
-            }
-            if (Array.isArray(value)) {
-                result = value.map(item => {
-                    return filterValues(item);
-                });
-            }
-        }
-        return result;
-    }
-    function filterObjectValues(obj) {
-        let filteredObject = {};
-        Object.keys(obj).forEach(key => {
-            filteredObject[key] = filterValues(obj[key]);
-        });
-        return filteredObject;
-    }
-
     return function(key, value) {
         if (typeof value === 'function') {
             return;
@@ -39,7 +12,7 @@ const factory = globals => {
         }
 
         // Store value for later use by jsContext
-        globals.storage.inject[key] = filterValues(value);
+        globals.storage.inject[key] = value;
     };
 };
 
