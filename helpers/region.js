ac7efbf0311b91ac539c4a9809cf899c6dd5d522
Kevin Chu <kevin.chu@bigcommerce.com>
2021-06-28 17:36:12 -0700

    MERC-7830 region name localization support

--- helpers/region.js
+++ helpers/region.js
@@ -3,13 +3,15 @@
 const factory = globals => {
     return function(params) {
         let regionId = params.hash.name;
+        let regionTranslation = params.hash.translation;
         let contentRegions = globals.getContent();
 
         if (!contentRegions) {
             return '';
         }
+        const translationDataAttribute = regionTranslation ? ` data-content-region-translation="${regionTranslation}"` : '';
 
-        const content = `<div data-content-region="${regionId}">${contentRegions[regionId] || ''}</div>`;
+        const content = `<div data-content-region="${regionId}"${translationDataAttribute}>${contentRegions[regionId] || ''}</div>`;
 
         return new globals.handlebars.SafeString(content);
     };
