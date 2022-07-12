c36c0a041ccb3c8e4fe517ca206bf18a0d5dd631
Evan Johnson <92830430+bc-evan-johnson@users.noreply.github.com>
2022-07-12 02:21:16 -0700

    patch for helper-date 0.2.3 compatibility (#180)

--- spec/helpers/moment.js
+++ spec/helpers/moment.js
@@ -3,6 +3,7 @@ const Lab = require('lab'),
     describe = lab.experiment,
     it = lab.it,
     testRunner = require('../spec-helpers').testRunner;
+const moment = require('moment');
 
 describe('moment helper', function () {
     const runTestCases = testRunner({});
@@ -13,7 +14,28 @@ describe('moment helper', function () {
             {
                 input: `{{moment "1 year ago" "YYYY"}}`,
                 output: `${now.getFullYear() - 1}`,
+            }
+        ], done);
+    });
+
+    it('is backwards compatible with helper-date 0.2.3 null-argument behavior', (done) => {
+        runTestCases([
+            {
+                input: `{{moment format="YYYY"}}`,
+                output: moment().format('YYYY'),
             },
+            {
+                input: `{{moment null null}}`,
+                output: moment().format('MMMM DD, YYYY'),
+            },
+            {
+                input: `{{moment undefined undefined}}`,
+                output: moment().format('MMMM DD, YYYY'),
+            },
+            {
+                input: `{{moment "now" format="YYYY"}}`,
+                output: `Invalid date`,
+            }
         ], done);
     });
 
