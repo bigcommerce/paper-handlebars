098eecbc80cf4da4430651283ef1fb34f87c9591
Rafa Avila <107151606+rafa-avila-bc@users.noreply.github.com>
2022-08-18 16:22:11 -0500

    [STRF-9949] paper-handlebars - cdn helper, add new resourceHints attribute (#189)

--- helpers/cdn.js
+++ helpers/cdn.js
@@ -1,6 +1,26 @@
 'use strict';
 
-const factory = require('./lib/cdnify');
+const cdnify = require('./lib/cdnify');
+const {addResourceHint} = require('./lib/resourceHints');
+
+const factory = globals => {
+    const cdn = cdnify(globals);
+
+    return function (path) {
+        const fullPath = cdn(path);
+
+        const options = arguments[arguments.length - 1];
+        if (options.hash.resourceHint) {
+            addResourceHint(
+                globals,
+                fullPath,
+                options.hash.resourceHint
+            );
+        }
+
+        return fullPath;
+    };
+};
 
 module.exports = [{
     name: 'cdn',
