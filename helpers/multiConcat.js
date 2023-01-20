329fbb73d5cfa963b6735047205fdacffb22b91a
Julia Chen <julia.chen@arcticleaf.io>
2023-01-20 17:49:57 -0500

    remove validate
    since join could accept all types of data

--- helpers/multiConcat.js
+++ helpers/multiConcat.js
@@ -1,7 +1,4 @@
 'use strict';
-const _ = require('lodash');
-const common = require('./lib/common.js');
-const utils = require('handlebars-utils');
 
 /**
  * Concats multi values, primarily used as a subhelper
@@ -15,25 +12,7 @@ const factory = globals => {
         // Take the last arg which is a Handlebars options object out of args array
         args.pop();
 
-        // Check if all the args are valid / truthy
-        const result = _.every(args, function (arg) {
-            if (utils.isArray(arg)) {
-                return !!arg.length;
-            }
-            // If an empty object is passed, arg is false
-            else if (utils.isEmpty(arg) && utils.isObject(arg)) {
-                return false;
-            }
-            // Everything else
-            else {
-                return !!arg;
-            }
-        });
-
-        if (result) {
-            return args.join('');
-        }
-
+        return args.join('');
     };
 
 };
--- spec/helpers/multiConcat.js
+++ spec/helpers/multiConcat.js
@@ -9,7 +9,8 @@ describe('multiConcat helper', function() {
     const context = {
         string1: "First",
         string2: "Second",
-        string3: "Third"
+        string3: "Third",
+        string4: "Fourth"
     };
 
     const runTestCases = testRunner({context});
@@ -19,6 +20,27 @@ describe('multiConcat helper', function() {
             {
                 input: '{{multiConcat string1 string2 string3}}',
                 output: 'FirstSecondThird',
+            },
+            {
+                input: '{{multiConcat string1 string2 string3 string4}}',
+                output: 'FirstSecondThirdFourth',
+            }
+        ], done);
+    });
+
+    it('should accept string, number, boolean, empty', function(done) {
+        runTestCases([
+            {
+                input: '{{multiConcat "First" 2}}',
+                output: 'First2',
+            },
+            {
+                input: '{{multiConcat string1 3 "" "4" true}}',
+                output: 'First34true',
+            },
+            {
+                input: '{{multiConcat string1 3 false "" "4" true}}',
+                output: 'First3false4true',
             }
         ], done);
     });
