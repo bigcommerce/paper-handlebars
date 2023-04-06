897482a14e75d7b32aa5f3220c8b33848d9fa074
Rafa Avila <rafael.avila@bigcommerce.com>
2023-04-06 14:56:10 -0600

    fix: PR fixes

--- spec/helpers/3p/markdown.js
+++ spec/helpers/3p/markdown.js
@@ -1,26 +1,26 @@
-// const Code = require('code');
-// const Lab = require('lab');
-// const lab = exports.lab = Lab.script();
-// const expect = Code.expect;
-// const it = lab.it;
-// const describe = lab.describe
-//
-// const { buildRenderer } = require('../../spec-helpers');
-// const renderer = buildRenderer();
-// const hbs = renderer.handlebars;
-// const helpers = require('../../../helpers/3p/markdown');
-//
-// Object.keys(helpers).forEach(key => {
-//     hbs.registerHelper(key, helpers[key]);
-// });
-//
-//
-// describe('markdown', function() {
-//   describe('markdown', function() {
-//     it('should render markdown using the {{#markdown}} block helper', function(done) {
-//       var template = hbs.compile('{{#markdown}}## {{../title}}{{/markdown}}');
-//       expect(template({title: 'Markdown Test'})).to.equal('<h2>Markdown Test</h2>\n');
-//       done();
-//     });
-//   });
-// });
+const Code = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+const expect = Code.expect;
+const it = lab.it;
+const describe = lab.describe
+
+const { buildRenderer } = require('../../spec-helpers');
+const renderer = buildRenderer();
+const hbs = renderer.handlebars;
+const helpers = require('../../../helpers/3p/markdown');
+
+Object.keys(helpers).forEach(key => {
+    hbs.registerHelper(key, helpers[key]);
+});
+
+
+describe('markdown', function() {
+  describe('markdown', function() {
+    it('should render markdown using the {{#markdown}} block helper', function(done) {
+      var template = hbs.compile('{{#markdown}}## {{../title}}{{/markdown}}');
+      expect(template({title: 'Markdown Test'})).to.equal('<h2>Markdown Test</h2>\n');
+      done();
+    });
+  });
+});
