0ecebd3198739abd1877150062536ba53b2f513a
Hunter Leachman <hunter.leachman@j97cm2n24k.lan>
2024-05-24 17:35:29 -0500

    chore(storefront): STRF-11941 Helper should just return nonce in quotes

--- helpers/nonce.js
+++ helpers/nonce.js
@@ -4,7 +4,7 @@ const factory = globals => {
     return function() {
         const params = globals.getRequestParams();
         if (params && params.security && params.security.nonce) {
-            return new globals.handlebars.SafeString(` nonce="${params.security.nonce}"`);
+            return new globals.handlebars.SafeString(`"${params.security.nonce}"`);
         }
         return ''
     };
