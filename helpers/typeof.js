96d40e0b7931802557727d52f2d696731fc6cdb8
Jordan <34638a@users.noreply.github.com>
2025-04-01 12:55:45 +1000

    Update helpers/typeof.js
    Co-authored-by: Evan Johnson <92830430+bc-evan-johnson@users.noreply.github.com>

--- helpers/typeof.js
+++ helpers/typeof.js
@@ -2,8 +2,12 @@
 const common = require('./lib/common.js');
 
 const factory = globals => {
-    return function(data) {
-        data = common.unwrapIfSafeString(globals.handlebars, data);
+      return function(data) {
+            arguments.pop(); // remove Options
+            if (arguments.length !== 1) {
+                throw new ValidationError("Handlebars helper 'typeof' requires exactly one argument.");
+            }
+            data = common.unwrapIfSafeString(globals.handlebars, data);
         return typeof data;
     };
 };
