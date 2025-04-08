a5df809ba88ee5e1785b21cd5e3f2e5ccb85b3ef
Jordan Ramsay <jordanr123@bigpond.com>
2025-04-08 14:49:24 +1000

    Modifications to typeof.js: - Add code to handle unacceptable states, now the function is only usable as an inline helper, and the function is only able to accept a single value of any standard type as input.
    New tests required for block helper and extra parameters exception states.

--- helpers/typeof.js
+++ helpers/typeof.js
@@ -3,15 +3,18 @@ const common = require('./lib/common.js');
 const {ValidationError} = require("../lib/errors");
 
 const factory = globals => {
-    return function (data) {
-        arguments.pop(); // remove Options
-        if (arguments.length !== 1) {
-            throw new ValidationError("Handlebars helper 'typeof' requires exactly one argument.");
+    return function (value) {
+        const options = arguments[arguments.length - 1];
+        if (options.fn) {
+            throw new ValidationError("Handlebars helper 'typeof' is not a block function.");
+        }
+        if (arguments.length-1 !== 1) {
+            throw new ValidationError("Handlebars helper 'typeof' requires exactly one argument. Provided: " + (arguments.length-1));
         }
         // Possible future issue due to type conversion: future implementation of flag to disable unwrap may be advisable.
         // TODO: Add flag to disable unwrap
-        data = common.unwrapIfSafeString(globals.handlebars, data);
-        return typeof data;
+        const toTest = common.unwrapIfSafeString(globals.handlebars, value);
+        return typeof toTest;
     };
 };
 
