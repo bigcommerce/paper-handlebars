2b7031bc3cef595fffd911fde95cf90adbcbd530
Jairo Panduro <jairo.panduro@bigcommerce.com>
2025-04-11 11:46:58 +0200

    Revert "fix: STRF-13111 Add stack trace to RenderError; Add knownHelpers (#354)"
    This reverts commit 5b825012b3a4c1106af40ab5e86789326f6b87a1.

--- helpers/dynamicComponent.js
+++ helpers/dynamicComponent.js
@@ -8,16 +8,23 @@ const factory = globals => {
             return;
         }
 
+        console.log(path);
         // prevent access to __proto__
         // or any hidden object properties
         path = path.replace('__', '');
+        console.log(path);
 
         // We don't want a slash as a prefix
         if (path[0] === '/') {
             path = path.substr(1);
         }
+        console.log(this['partial']);
 
         path = Path.join(path, this['partial']);
+        console.log('path');
+        console.log(path);
+        console.log(Object.keys(globals.handlebars.partials));
+        console.log(globals.handlebars.partials[path]);
 
         if (globals.handlebars.partials[path] && Object.hasOwnProperty.call(globals.handlebars.partials, path)) {
             return globals.handlebars.partials[path](this);
